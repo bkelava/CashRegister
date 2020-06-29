039ddd0bbc6b81cc9a090d5c483e1e062b2c18f7
bkelava <59897801+bkelava@users.noreply.github.com>
2020-06-29 02:28:19 +0200

    invoice UI finished

--- app/src/main/java/hr/ferit/bozidarkelava/cashregister/viewModels/InvoiceViewModel.kt
+++ app/src/main/java/hr/ferit/bozidarkelava/cashregister/viewModels/InvoiceViewModel.kt
@@ -1,2 +1,13 @@
 package hr.ferit.bozidarkelava.cashregister.viewModels
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class InvoiceViewModel: ViewModel() {
+
+    val totalPrice = MutableLiveData<Double>()
+
+    fun setTotalPrice(price: Double) {
+        this.totalPrice.value = price
+    }
+}
